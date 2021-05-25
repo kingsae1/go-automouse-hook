50fe5cfd78b80df08ed28dc263b6605ec77e3214
kingsae <kingsae1@naver.com>
2021-05-25 13:49:16 +0900

    [Init] auto mouse

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-"# go.automouse.hook" 
+# go.automouse.hook
+회사 프로그램 보안프로그램을 이용하다보면, 강제로 N분 이후에 이벤트가 없으면
+절전모드로 변환되는 경우가 발생하는데 이를 방지하기 위해 5분동안 키, 마우스 이벤트가
+발생하지 않으면 강제로 발생시켜주는 프로그램이다
+
+## Run Go script (test)
+```
+go run mouse.go
+```
+
+## Build Go script
+```
+go build mouse.go
+```
---
+++ mouse.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/go-vgo/robotgo"
+	hook "github.com/robotn/gohook"
+)
+
+type ticker struct {
+	period time.Duration
+	ticker time.Ticker
+}
+
+func createTicker(period time.Duration) *ticker {
+	return &ticker{period, *time.NewTicker(period)}
+}
+func (t *ticker) resetTicker() {
+	t.ticker = *time.NewTicker(t.period)
+}
+
+func main() {
+	add()
+	low()
+	// event()
+}
+
+func add() {
+	data, error := ioutil.ReadFile("version.txt")
+
+	if error != nil {
+		fmt.Println(error)
+		data = []byte("Please check version.txt")
+	}
+
+	fmt.Println(" #######################################")
+	fmt.Println(`	 ⣠⣤⡾⠿⠿⠿⢷⣤⣀⠀⠀⠀⠀⠀⣀⣤⡾⠿⠿⠿⢷⣤⣄
+	⣤⡿⠋⠀⠀⠀⠀⠀⠙⣿⣄⠀⠀⠀⣠⣿⠋⠁⠀⠀⠀⠀⠙⢿⣤
+	⣿⡇⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⠿⠀⠀⠀⠀⠀⠀⠀⢸⣿
+	⠻⣷⣄⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⢠⣼⠟
+	⠀⠙⢿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⠋
+	⠀⠀⠀⢹⣿⠀⢀⣴⣿⠀⠀⠀⠀⠀⠀⠀⣿⣦⡄⠀⣿⡏
+	⠀⠀⠀⢸⣿⠀⠘⢿⣿⠀⠀⠀⠀⠀⠀⠀⣿⡿⠃⠀⣿⡇
+	⠀⠀⠀⠘⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⠃
+	⠀⠀⠀⢠⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣄
+	⠀⠀⢸⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⣀⣾⣿⣿⣿⣿⣿⡇
+	⠀⠀⠀⢹⣿⣿⣿⡟⠛⢿⣷⣄⠀⣠⣶⡿⠛⢻⣿⣿⣿⡏⠁
+	⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠙⠿⠿⠿⠋⠀⠀⠀⠈⠉⠉`)
+	fmt.Println(" ########### Mouse Automover ###########")
+	fmt.Println(" Author : kingsae1004@gmail.com")
+	fmt.Println(" Version : " + string(data))
+	fmt.Println(" #######################################")
+	fmt.Println(" Please press 'q' to start event hook !")
+	robotgo.EventHook(hook.KeyDown, []string{"q"}, func(e hook.Event) {
+		fmt.Println(" Detect Keyboard and Mouse event hook !")
+		robotgo.EventEnd()
+	})
+
+	// robotgo.EventHook(hook.MouseUp, []string{"mleft"}, func(e hook.Event) {
+	// fmt.Println("mouse-left")
+	// robotgo.EventEnd()
+	// })
+
+	s := robotgo.EventStart()
+	<-robotgo.EventProcess(s)
+}
+
+func low() {
+	EvChan := hook.Start()
+	defer hook.End()
+
+	ticker := createTicker(5 * 60 * time.Second)
+	ticker.resetTicker()
+
+	color := []string{"magenta", "cyan", "white", "yellow"}
+	colorIndex := 0
+
+	s := spinner.New(spinner.CharSets[35], 500*time.Millisecond) // Build our new spinner
+	s.Prefix = " Detecting Event Hook : "
+	s.Color(color[colorIndex])
+
+	n := 1
+	go func() {
+		for n > 0 {
+			select {
+			//case <-done:
+			//	return
+			case <-ticker.ticker.C:
+				// fmt.Println("Move Scroll mouse")
+				colorIndex += 1
+
+				if colorIndex >= len(color) {
+					colorIndex = 0
+				}
+				s.Color(color[colorIndex])
+
+				robotgo.ScrollMouse(10, "up")
+			}
+		}
+	}()
+	for ev := range EvChan {
+		if ev.Kind != 0 {
+			// for n > 0 {
+			s.Reverse() // Reverse the direction the spinner is spinning
+			s.Restart()
+			time.Sleep(10 * time.Second)
+			s.Stop()
+			ticker.resetTicker()
+		}
+	}
+}
+
+// func event() {
+// 	ok := robotgo.AddEvents("q")
+// 	if ok {
+// 		fmt.Println("add events...")
+// 	}
+
+// 	mleft := robotgo.AddEvent("mleft")
+// 	if mleft {
+// 		fmt.Println("you press... ", "mouse left button")
+// 	}
+
+// }
